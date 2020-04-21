eca20fd316c838c11b8ed71715b5fe759f3e3d74
Victor Zhuravlev <mocambe@mail.ru>
2020-04-21 23:16:50 +0300

    step 4 completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { string, mixed } from 'yup';
+import { uniqueId, noop } from 'lodash';
 import axios from 'axios';
 import i18next from 'i18next';
 import parse from './parse';
@@ -40,6 +41,25 @@ const handleChange = ({ target: { value } }) => {
   state.error = null;
 };
 
+const addTimer = (item) => setTimeout(() => {
+  const { url, id } = item;
+  const processedUrl = `${corsApiUrl}/${url}`;
+  axios(processedUrl)
+    .then(({ data, status }) => ({ data, status }))
+    .then(({ data, status }) => {
+      if (status === 200) {
+        return parse(data);
+      }
+      throw new Error(`Request status ${status}`);
+    })
+    .then((parsed) => {
+      const updatedRssItems = state.rssItems.map((i) => (i.id === id ? { ...i, parsed } : i));
+      state.rssItems = updatedRssItems;
+    })
+    .catch(() => noop());
+  addTimer(item);
+}, 20000);
+
 const handleSubmit = (e) => {
   e.preventDefault();
   const { rssItems } = state;
@@ -49,8 +69,9 @@ const handleSubmit = (e) => {
   axios(url)
     .then(({ data }) => data)
     .then((data) => parse(data))
-    .then((newItem) => {
-      const newRssItem = { ...newItem, url: rssUrl };
+    .then((parsed) => {
+      const newRssItem = { ...parsed, url: rssUrl, id: uniqueId() };
+      addTimer(newRssItem);
       state.rssItems = [...rssItems, newRssItem];
       state.success = i18next.t('success');
     })
@@ -61,6 +82,8 @@ const handleSubmit = (e) => {
   state.valid = false;
   form.reset();
 };
+
+
 const app = async () => {
   await i18next.init({ lng: 'en', debug: true, resources });
   input.addEventListener('input', handleChange);
--- src/render.js
+++ src/render.js
@@ -1,10 +1,35 @@
 import { last } from 'lodash';
 import { watch } from 'melanke-watchjs';
-import buildArticle from './buildArticle';
+// import buildArticle from './buildArticle';
 
 const feedContainer = document.querySelector('.feed-container');
 const input = document.querySelector('.form-control');
 const feedbackContainer = document.querySelector('.feedback');
+
+const buildListItem = ({ text, link }) => {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  a.innerText = text;
+  a.setAttribute('href', link);
+  li.append(a);
+  return li;
+};
+
+const buildArticle = (rss) => {
+  const { title, description, rssLinks } = rss;
+  const article = document.createElement('article');
+  const ul = document.createElement('ul');
+  ul.classList.add('list-unstyled');
+  const listItems = rssLinks.map(buildListItem);
+  ul.append(...listItems);
+  const h4 = document.createElement('h4');
+  h4.innerText = title;
+  const p = document.createElement('p');
+  p.innerText = description;
+  article.append(h4, p, ul);
+  return article;
+};
+
 
 const toggleInputClassnames = (state) => {
   const isInputValidated = input.classList.contains('is-valid') || input.classList.contains('is-invalid');
@@ -43,9 +68,17 @@ export default (state) => {
   });
 
 
-  watch(state, 'rssItems', () => {
-    const newItem = last(state.rssItems);
-    const article = buildArticle(newItem);
-    feedContainer.append(article);
+  watch(state, 'rssItems', (_prop, _action, newItems, oldItems) => {
+    if (newItems.length !== oldItems.length) {
+      const newItem = last(newItems);
+      const article = buildArticle(newItem);
+      feedContainer.append(article);
+      return;
+    }
+    feedContainer.innerHTML = '';
+    newItems.forEach((item) => {
+      const article = buildArticle(item);
+      feedContainer.append(article);
+    });
   });
 };
